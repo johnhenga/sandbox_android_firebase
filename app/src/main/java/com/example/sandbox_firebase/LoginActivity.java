776e8f88bfa2802c9bb2193ee98ec2ac66a73aa6
John Heng <john@gosoft.co.th>
2025-07-19 13:38:54 +0700

    routine commit

---
+++ app/src/main/java/com/example/sandbox_firebase/LoginActivity.java
@@ -0,0 +1,39 @@
+package com.example.sandbox_firebase;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import com.google.firebase.auth.FirebaseAuth;
+import com.example.sandbox_firebase.databinding.ActivityLoginBinding;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "EmailPassword";
+
+    private ActivityLoginBinding mBinding;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+
+
+}
